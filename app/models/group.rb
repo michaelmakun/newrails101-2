5a298484d6451a977172874c751d0a0379a12098
makun <michaelmakun@aliyun.com>
2017-03-11 19:47:11 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
   has_many :posts
+
+  has_many :group_relationships
+  has_many :participated_groups, through: :group_relationships, source: :user
 end
