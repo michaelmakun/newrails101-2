b4f1c957cb4085c3460af00150761db6b123dc9c
makun <michaelmakun@aliyun.com>
2017-03-11 19:53:15 +0800

    change group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,5 @@ class Group < ApplicationRecord
   has_many :posts
 
   has_many :group_relationships
-  has_many :participated_groups, through: :group_relationships, source: :user
+  has_many :members, through: :group_relationships, source: :user
 end
